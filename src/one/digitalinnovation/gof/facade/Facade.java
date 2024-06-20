c67df018d8f11086fe4dc987c592cbad5d8c2716
Ari Alberto <154787080+AriRJ@users.noreply.github.com>
2024-06-19 23:29:21 -0300

    Update Facade.java

--- src/one/digitalinnovation/gof/facade/Facade.java
+++ src/one/digitalinnovation/gof/facade/Facade.java
@@ -5,10 +5,18 @@ import subsistema2.cep.CepApi;
 
 public class Facade {
 
-	public void migrarCliente(String nome, String cep) {
-		String cidade = CepApi.getInstancia().recuperarCidade(cep);
-		String estado = CepApi.getInstancia().recuperarEstado(cep);
-		
-		CrmService.gravarCliente(nome, cep, cidade, estado);
-	}
+    private CrmService crmService;
+    private CepApi cepApi;
+
+    public Facade() {
+        this.crmService = CrmService.getInstancia();
+        this.cepApi = CepApi.getInstancia();
+    }
+
+    public void migrarCliente(String nome, String cep) {
+        String cidade = cepApi.recuperarCidade(cep);
+        String estado = cepApi.recuperarEstado(cep);
+        
+        crmService.gravarCliente(nome, cep, cidade, estado);
+    }
 }
